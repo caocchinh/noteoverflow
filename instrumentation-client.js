47fc47d0ccfe7200b04736ae598c75b935ada763
caocchinh <chinhcaocu@gmail.com>
2025-08-24 02:25:55 +0700

    fix: remove unused environment variables and update PostHog initialization with static values

--- instrumentation-client.js
+++ instrumentation-client.js
@@ -1,6 +1,6 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+posthog.init("phc_5YG6xf8EsOVyaYg3xI4hWj47Mw3pNVDBQz5IEbxcdJ1", {
+  api_host: "https://us.i.posthog.com",
   defaults: "2025-05-24",
 });
